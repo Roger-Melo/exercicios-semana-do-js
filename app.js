04b851a7d95913d8034eeff44337ed0e2d9e8a01
Roger Melo <rogermelodev@gmail.com>
2022-04-26 13:00:32 -0300

    Add remainder operator hint

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 
   - Gere um novo array com apenas os números ímpares do array abaixo e exiba 
     o novo array no console.
+
+  Dica: pesquise por remainder operator (%).
 */
 
 const randomNumbers = [10, 30, 15, 25, 50, 40, 5]
